352b3ff41f4cfbf77f3f4f10a5348b406a4648ad
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-21 22:46:13 +0600

    Refactor: update import path for Toggle component in TipSettings; remove unused NumberField import in StockThreshold

--- spa/admin/src/pages/StockThreshold.jsx
+++ spa/admin/src/pages/StockThreshold.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import SettingSkeleton from "../../common/Skeletons/SettingSkalaton";
 import { SaveButtonIcon, SaveChangesIcon, WarningIcon } from "../../common/Svgs";
-import Toggle from "../common/Toggle";
 import SectionHeader from "../../common/SectionHeader";
+import NumberField from "../../common/NumberField";
+import Toggle from "../../common/Toggle";
 
 
 const StockThreshold = () => {
