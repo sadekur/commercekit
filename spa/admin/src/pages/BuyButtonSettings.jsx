aae775b285318f7ec0c5db6b4ac28310ebbd533b
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-21 22:52:54 +0600

    Add: create DimensionFields component file

--- spa/admin/src/pages/BuyButtonSettings.jsx
+++ spa/admin/src/pages/BuyButtonSettings.jsx
@@ -31,20 +31,6 @@ const DEFAULTS = {
     button_padding:          { top: "", right: "", bottom: "", left: "" },
 };
 
-const DimensionFields = ({ value, onChange }) => (
-    <div className="flex gap-3 flex-wrap">
-        {["top", "right", "bottom", "left"].map((side) => (
-            <label key={side} className="flex flex-col items-center gap-1">
-                <span className="text-[10px] text-gray-400 capitalize">{side}</span>
-                <SmallNumberInput
-                    value={value[side]}
-                    onChange={(v) => onChange({ ...value, [side]: v })}
-                />
-            </label>
-        ))}
-    </div>
-);
-
 const SaveRow = ({ isSaving }) => (
     <div className="flex items-center gap-3 pt-1">
         <button
