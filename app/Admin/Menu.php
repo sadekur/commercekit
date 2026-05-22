3a68d7fe158ee8930f4fa4fc8c116e9dbf4662d5
Sadekur <shadekur.rahman60@gmail.com>
2026-05-22 22:43:54 +0600

    Add hidden feature hashes to localized admin script data for improved settings management

--- app/Admin/Menu.php
+++ app/Admin/Menu.php
@@ -8,8 +8,7 @@ class Menu {
     use Hookable;
 
     function __construct() {
-        $this->action( 'admin_menu',             [ $this, 'add_admin_menu' ] );
-        $this->action( 'admin_enqueue_scripts',  [ $this, 'enqueue_menu_visibility' ] );
+        $this->action( 'admin_menu', [ $this, 'add_admin_menu' ] );
     }
 
     public function add_admin_menu() {
