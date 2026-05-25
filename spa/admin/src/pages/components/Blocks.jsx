e622d3ea56e5cbaac92b18b848b063b228469db6
Sadekur <shadekur.rahman60@gmail.com>
2026-05-25 12:28:30 +0600

    fix Blocks.jsx to use SkeletonCard instead of the plain Loading... text

--- spa/admin/src/pages/components/Blocks.jsx
+++ spa/admin/src/pages/components/Blocks.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CommonHeader from "../../../common/CommonHeader";
+import SkeletonCard from "../../../common/Skeletons/SkeletonCard";
 
 const Blocks = () => {
 	const url = `${COMMERCEKIT.apiurl}/block-register-save`;
