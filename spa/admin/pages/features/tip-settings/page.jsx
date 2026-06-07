ab6dab373d215e72edf9d710a0ecc233f6a6c339
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-06-07 22:24:13 +0600

    Add: import SaveButtonIcon and update saving indicator in TipSettings component

--- spa/admin/pages/features/tip-settings/page.jsx
+++ spa/admin/pages/features/tip-settings/page.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import TabPageSkeleton from "../../../common/Skeletons/TabPageSkeleton";
-import { SaveButtonIcon, WarningIcon } from "../../../common/Svgs";
+import { SaveButtonIcon, SaveChangesIcon, WarningIcon } from "../../../common/Svgs";
 import Toggle from "../../../common/Toggle";
 import SettingRow from "../../../common/SettingRow";
 import FancyInput from "../../../common/FancyInput";
@@ -283,9 +283,7 @@ const TipSettings = () => {
                             </>
                         ) : (
                             <>
-                                <svg className="w-3.5 h-3.5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"/>
-                                </svg>
+                                {SaveChangesIcon}
                                 Save Changes
                             </>
                         )}
