fc939ca259865c132cbeb125856288734499817c
Sadekur <shadekur.rahman60@gmail.com>
2026-07-07 10:48:27 +0600

    add carousel style class to swiper element for improved layout

--- blocks/category-products-slider/render/HtmlTrait.php
+++ blocks/category-products-slider/render/HtmlTrait.php
@@ -176,7 +176,7 @@ trait CslHtmlTrait {
         $wrap_cls    = 'ck-csl-wrap relative w-full' . ( $rtl ? '' : ' ck-csl-rtl' ) . ( $show_pager ? ' ck-csl-has-pager' : '' ) . ( $preloader ? ' ck-csl-preloader' : '' );
         $outer_cls   = 'ck-csl-outer relative' . ( $show_nav ? ' ck-has-nav' : '' );
         $nav_btn_cls = 'ck-csl-nav-btn flex items-center justify-center cursor-pointer pointer-events-auto transition-[background,color,border-color] duration-200 shrink-0 focus:outline focus:outline-2 focus:outline-[#0073aa] focus:outline-offset-2';
-        $swiper_cls  = 'swiper ck-csl-swiper overflow-hidden' . ( $show_pager ? ' pb-10' : '' );
+        $swiper_cls  = 'swiper ck-csl-swiper overflow-hidden' . ( $show_pager ? ' pb-10' : '' ) . $this->carousel_style_class();
 
         ob_start();
         if ( $show_nav ) : ?>
